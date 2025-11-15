950a3ce76a4d91746e930ed25252b84a0704975e
Siddarth Gera <gerasiddharth@gmail.com>
2025-11-15 22:42:15 +0000

    initial commit

---
+++ buildapp/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace buildapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
